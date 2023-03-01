0718f4af4fdd39ebcd44977594da0c6c4a7ec7f7
Piryanshu88 <deepubisht2004@gmail.com>
2023-03-01 16:48:54 +0530

    adding query for category

--- router/men.router.js
+++ router/men.router.js
@@ -3,12 +3,113 @@ const { MenModel } = require("../models/product.model");
 const productRouter = express.Router();
 
 productRouter.get("/mens", async (req, res) => {
+  const category = req?.query?.category;
+  const page = req?.query?.page;
+  const limit = req?.query?.limit || 15;
+  const sort = req?.query?.sortby;
+  let min_price = req?.query?.min_price;
+  let max_price = req?.query?.max_price;
+  let s;
+  if (sort == "asc") {
+    s = 1;
+  } else if (sort == "desc") {
+    s = -1;
+  }
   try {
-    const product = await MenModel.find().limit(15);
-    const productlength = await MenModel.find().count();
-    res
-      .status(201)
-      .json({ data: product, status: "success", totalCount: productlength });
+    if (category && min_price && max_price) {
+      min_price = Number(min_price);
+      max_price = Number(max_price);
+      console.log(min_price, max_price);
+      const product = await MenModel.find({
+        category: category,
+        price: { $gte: min_price, $lte: max_price },
+      }).limit(limit);
+      const productlength = await MenModel.find({
+        category: category,
+        price: { $gte: min_price, $lte: max_price },
+      }).count();
+
+      res.status(201).json({
+        data: product,
+        status: "success",
+        totalCount: productlength,
+      });
+    } else if (min_price && max_price) {
+      min_price = Number(min_price);
+      max_price = Number(max_price);
+      const product = await MenModel.find({
+        price: { $gte: min_price, $lte: max_price },
+      }).limit(limit);
+      const productlength = await MenModel.find({
+        price: { $gte: min_price, $lte: max_price },
+      }).count();
+
+      res.status(201).json({
+        data: product,
+        status: "success",
+        totalCount: productlength,
+      });
+    } else if (min_price) {
+      min_price = Number(min_price);
+
+      const product = await MenModel.find({
+        price: { $gte: min_price },
+      }).limit(limit);
+      const productlength = await MenModel.find({
+        price: { $gte: min_price },
+      }).count();
+
+      res.status(201).json({
+        data: product,
+        status: "success",
+        totalCount: productlength,
+      });
+    } else if (max_price) {
+      min_price = Number(min_price);
+      max_price = Number(max_price);
+      const product = await MenModel.find({
+        price: { $lte: max_price },
+      }).limit(limit);
+      const productlength = await MenModel.find({
+        price: { $lte: max_price },
+      }).count();
+
+      res.status(201).json({
+        data: product,
+        status: "success",
+        totalCount: productlength,
+      });
+    } else if (category && sort) {
+      const product = await MenModel.find({ category: category })
+        .limit(limit)
+        .sort({ price: s });
+      const productlength = await MenModel.find({
+        category: category,
+      }).count();
+      res.status(201).json({
+        data: product,
+        status: "success",
+        totalCount: productlength,
+      });
+    } else if (category) {
+      const product = await MenModel.find({ category: category }).limit(limit);
+      const productlength = await MenModel.find({
+        category: category,
+      }).count();
+      res.status(201).json({
+        data: product,
+        status: "success",
+        totalCount: productlength,
+      });
+    } else {
+      const product = await MenModel.find().limit(limit);
+      const productlength = await MenModel.find().count();
+      res.status(201).json({
+        data: product,
+        status: "success",
+        totalCount: productlength,
+      });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Something went wrong", status: "error" });
